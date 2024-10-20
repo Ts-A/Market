60c15433617e7d2dfe5a096f7163e4fdfa742d09
ajeet <ajeet.tumuluru@gmail.com>
2024-10-20 22:38:34 +0530

    setup concurrently

--- backend/main/clients/UserClient.js
+++ backend/main/clients/UserClient.js
@@ -1,7 +1,7 @@
 import grpc from "@grpc/grpc-js";
 import protoLoader from "@grpc/proto-loader";
 
-const PROTO_PATH = "./protos/user.proto";
+const PROTO_PATH = "../protos/user.proto";
 
 const userProtoPackageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
