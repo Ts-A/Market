8777297981938f30e01dbc6c8ef1dc421d1d3dc2
ajeet <ajeet.tumuluru@gmail.com>
2024-10-23 20:32:27 +0530

    error handling create product

--- backend/products-service/helpers/createProduct.js
+++ backend/products-service/helpers/createProduct.js
@@ -5,47 +5,33 @@ import jwt from "jsonwebtoken";
 import redis from "../configs/RedisClient.js";
 
 export default async (call, callback) => {
-  const { category, price, stock, name, token } = call.request;
+  try {
+    const { category, price, stock, name, token } = call.request;
+    if (!token) throw new Error("JWT Missing");
+    var decoded;
 
-  if (!token)
-    return callback({
-      code: GRPC_STATUS.NOT_FOUND,
-      details: "Invalid token",
+    decoded = jwt.verify(token, "secret");
+    if (decoded.role !== "admin") throw new Error("Not admin");
+    if (!(await redis.sIsMember(decoded.id, decoded.sessionId)))
+      throw new Error("Session expired");
+
+    const product = await db.product.create({
+      data: {
+        id: uuidv4(),
+        category,
+        name,
+        price,
+        stock,
+      },
     });
 
-  var decoded;
+    if (!product) throw new Error("Unable to create a product");
 
-  try {
-    decoded = jwt.verify(token, "secret");
-    if (decoded.role !== "admin")
-      throw new Error({
-        message: "Not an admin",
-        code: GRPC_STATUS.PERMISSION_DENIED,
-      });
-    if (!(await redis.sIsMember(decoded.id, decoded.sessionId)))
-      throw new Error({
-        message: "Session expired",
-        code: GRPC_STATUS.NOT_FOUND,
-      });
+    return callback(null, product);
   } catch (error) {
     return callback({
-      code: error.code || GRPC_STATUS.NOT_FOUND,
-      details: error.message || "Session expired",
+      code: GRPC_STATUS.PERMISSION_DENIED,
+      details: error.message,
     });
   }
-
-  const product = await db.product.create({
-    data: {
-      id: uuidv4(),
-      category,
-      name,
-      price,
-      stock,
-    },
-  });
-
-  if (!product)
-    return callback({ code: status.ABORTED, details: "Something went wrong" });
-
-  return callback(null, product);
 };
